86a8f17e60a255b8d513bf21da5cd0ba3aaccb95
Anthony <antsal07@gmail.com>
2024-08-31 21:58:31 -0400

    Server Beginnings

--- UTR - Server/Scripts/Client.cs
+++ UTR - Server/Scripts/Client.cs
@@ -1,4 +1,66 @@
-﻿public class Client
-{
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
+public class Client
+{
+	private TCP tcp;
+	private int id;
+	//private Player player;
+	//private Account account;
+
+	public Client(TcpClient _tcpClient, int _id)
+	{
+		tcp = new(_tcpClient, this);
+		id = _id;
+	}
+
+	private class TCP
+	{
+		Client instance;
+		TcpClient tcpClient;
+		NetworkStream stream;
+
+		byte[] buffer;
+
+		public TCP(TcpClient _tcpClient, Client _instance)
+		{
+			instance = _instance;
+			tcpClient = _tcpClient;
+
+			buffer = new byte[10];
+
+			stream = tcpClient.GetStream();
+			
+			ReadStreamAsync();
+		}
+
+		private async Task ReadStreamAsync()
+		{
+			try
+			{
+				int _readLength = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+				if (_readLength <= 0)
+				{
+					// Disconnect
+					return;
+				}
+
+				StringBuilder sb = new(Encoding.ASCII.GetString(buffer));
+
+				while (stream.DataAvailable)
+				{
+					_readLength = stream.Read(buffer, 0, buffer.Length);
+					sb.Append(Encoding.ASCII.GetString(buffer, 0, _readLength));
+				}
+			}
+			catch (Exception)
+			{
+				// Disconnect
+				return;
+			}
+		}
+	}
 }
--- UTR - Server/Scripts/ServerManager.cs
+++ UTR - Server/Scripts/ServerManager.cs
@@ -1,8 +1,10 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 
 public partial class ServerManager : Node
 {
@@ -13,7 +15,7 @@ public partial class ServerManager : Node
 	private static List<int> ids = new();
 	private static Dictionary<int, Client> clients = new();
 
-	private static List<TcpClient> tpcClientQueue = new();
+	private static List<TcpClient> tcpClientQueue = new();
 
 	public override void _Ready()
 	{
@@ -22,11 +24,62 @@ public partial class ServerManager : Node
 		listener = new(IPAddress.Any, 6666);
 
 		listener.Start();
-		listener.BeginAcceptTcpClient(AcceptClientCallback, this);
+		listener.BeginAcceptTcpClient(AcceptClientCallback, null);
+
+		Print("Started");
 	}
 
-	private static void AcceptClientCallback(IAsyncResult result)
+	private static void AcceptClientCallback(IAsyncResult _result)
 	{
+		TcpClient _tempTcpClient;
 
+		try
+		{
+			_tempTcpClient = listener.EndAcceptTcpClient(_result);
+		}
+		catch (Exception)
+		{
+			listener.BeginAcceptTcpClient(AcceptClientCallback, null);
+			return;
+		}
+
+		tcpClientQueue.Add(_tempTcpClient);
+
+		listener.BeginAcceptTcpClient(AcceptClientCallback, null);
+	}
+
+	public override void _Process(double delta)
+	{
+		if (tcpClientQueue.Count > 0)
+		{
+			ClientQueueManage();
+		}
+	}
+
+	private static void ClientQueueManage()
+	{
+		while (tcpClientQueue.Count > 0)
+		{
+			TcpClient _curClient = tcpClientQueue[0];
+
+			int _id = 0;
+
+			while (ids.Contains(_id))
+			{
+				_id++;
+			}
+
+			Client _tempClient = new(_curClient, _id);
+			clients.Add(_id, _tempClient);
+			
+			playerCount++;
+
+			tcpClientQueue.RemoveAt(0);
+		}
+	}
+
+	public static void Print(string message)
+	{
+		GD.Print(message);
 	}
 }
