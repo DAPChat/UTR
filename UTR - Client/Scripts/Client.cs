673fb18547a2f73fce739cb53a1c4d28a4a0a24d
Anthony <antsal07@gmail.com>
2024-12-02 12:31:07 -0500

    gg

--- UTR - Client/Scripts/Client.cs
+++ UTR - Client/Scripts/Client.cs
@@ -19,7 +19,7 @@ public class Client
 
 	//10.72.101.156
 
-	public IPEndPoint end = new(IPAddress.Parse("10.72.102.113"), 6666);
+	public IPEndPoint end = new(IPAddress.Parse("10.72.103.23"), 6666);
 
 	public Client()
 	{
