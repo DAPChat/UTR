8b9687c143cfd5f0281b8c73da4b7d7a33a916ba
froggo_doggo <110413184+realfroggodoggo@users.noreply.github.com>
2024-12-16 12:05:12 -0500

    title swing add

--- UTR - Client/Scripts/SeizureProcedure.gd
+++ UTR - Client/Scripts/SeizureProcedure.gd
@@ -1,9 +1,13 @@
 extends CanvasLayer
 
-var flipFlop=2
+var flipFlop=1
+var flipInc=0
 
 func _on_timer_timeout() -> void:
-	%Title.position.y += flipFlop
-	flipFlop = -1*flipFlop
-	
+	%Title.position.y += flipFlop+(flipFlop*(0.25*flipInc))
+	flipInc += 1
+	if (flipInc==10):
+		flipFlop = -1*flipFlop
+		flipInc=0
+		
 	pass # Replace with function body.
